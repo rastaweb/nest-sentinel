0c495c55b7364e687a4afed84f62b6666eb28873
kamran <kamran.it404@gmail.com>
2025-10-15 21:57:30 +0330

    Update index.ts

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name("sentinel")
   .description("CLI for @rastaweb/nest-sentinel library")
-  .version("1.0.7");
+  .version("1.0.8");
 
 // Initialize database command
 program
